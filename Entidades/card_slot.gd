5cbeb0dba44798a2bb4d3c9d8fa0436507f22319
Atarox <80927839+Atarox228@users.noreply.github.com>
2025-09-24 15:55:06 -0300

    grilla armada, turno y contador puestos, empezando spawnerMostruos

--- Entidades/card_slot.gd
+++ Entidades/card_slot.gd
@@ -1,11 +1,29 @@
+# CardSlot.gd
 extends Node2D
+class_name CardSlot
 
+@export var is_occupied: bool = false
+var current_card: Node2D = null
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print($Area2D)
+func place_card(card: Node2D) -> bool:
+	if is_occupied:
+		return false
+	
+	current_card = card
+	is_occupied = true
+	
+	# Mover la carta a la posición del slot
+	card.reparent(self)
+	card.position = Vector2.ZERO
+	
+	return true
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func remove_card() -> Node2D:
+	if not is_occupied:
+		return null
+	
+	var card = current_card
+	current_card = null
+	is_occupied = false
+	
+	return card
