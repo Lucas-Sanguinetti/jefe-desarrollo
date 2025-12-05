bcae45abb4cd00437fd5651aabb2bad5ae8467ef
Lautaro-Galvez-Monge <80927839+Atarox228@users.noreply.github.com>
2025-12-05 17:04:46 -0300

    Revert "Feature/fixes rapidos"

--- singleton/weapon_deck.gd
+++ singleton/weapon_deck.gd
@@ -13,6 +13,7 @@ func _ready() -> void:
 	armas_reset = weapons_data_cards.duplicate(true)
 
 func draw():
+	push_warning("robe una carta")
 	var weapon:WeaponCardData = weapons_data_cards[0]
 	weapons_data_cards.remove_at(0)
 	return weapon
